70990166270cc067d05dfd6343518250a0238481
vinhluong <vinhde1@gmail.com>
2020-04-04 22:09:28 +0700

    Update show_detail.php

--- show_detail.php
+++ show_detail.php
@@ -14,17 +14,19 @@
     </head>
     <body>
         <div>
-            <h1>Knowing the UDID of my iOS device</h1>
+            <h1>Cen IOS Pubg Mobile Hack No JB</h1>
                 <p>UDID: <span class=mono id="theList"> <?php echo $_GET['UDID']; ?></span></p>
                  <button id="copyButton" onclick="myCopyFunction()">Copy UDID.</button>
                 <p>Device product: <?php echo $_GET['DEVICE_PRODUCT']; ?></p>
                 <p>Device version: <?php echo $_GET['DEVICE_VERSION']; ?></p>
                 <p>Device name: <?php echo $_GET['DEVICE_NAME']; ?></p>
 
-                <p>Step 2: Send the information by email:</p>
-                <p>
-                    <a href="mailto:?subject=<?php echo $subject ?>&body=<?php echo $body?>">Give me tap</a>
-                </p>
+                 <ul class="buttons">
+                <li><a href="https://www.facebook.com/k3ndzy" class="button wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".6s">Send Me</a></li>
+                <li><a href="https://is.gd/CenIOSYT" class="button button-border button-transparent wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".9s">Youtube</a></li>
+				<li><a href="https://is.gd/CenIOSGROUP" class="button button-border button-transparent wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".9s">Group Cen IOS</a></li>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/xrkGDmR-xEA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</ul>
         </div>
     </body>
 </html>
